5819db846a780dab3555349e122ec0d0e8d60c06
unknown <janicechungg@gmail.com>
2023-10-28 20:24:33 -0500

    Account tailwind

--- src/AccountPage.js
+++ src/AccountPage.js
@@ -1,77 +1,83 @@
-import React from 'react';
+import React from "react";
 
 function AccountPage() {
-    const nutrients = [
-        {nutrient: "Calories"},
-        {nutrient: "Fat"},
-        {nutrient: "Cholesterol"},
-        {nutrient: "Sodium"},
-        {nutrient: "Carbohydrate"},
-        {nutrient: "Dietary Fiber"},
-        {nutrient: "Protein"},
-        {nutrient: "Calcium"},
-        {nutrient: "Iron"},
-        {nutrient: "Potasium"},
-    ]
-    //const [option] = useState(nutrients);
-    return (
-        <body>
+  return (
+    <body>
+      <div>
+        <h1 className="font-bold text-center text-5xl mb-5">Welcome</h1>
+      </div>
+      <div className="flex flex-col justify-center content-center text-center">
         <div>
-            <h1>Welcome</h1>
+          <label htmlFor="restrictions" className="font-bold text-3xl">
+            Change your dietary restrictions here
+          </label>
+        </div>
+        <div>
+          <input
+            className="shadow appearance-none border rounded w-30% py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-5"
+            type="text"
+            id="restrictions"
+            name="restrictions"
+            placeholder="Enter restrictions"
+          ></input>
+        </div>
+        <div className ="mt-8">
+          <label htmlFor="dropdown">Select Diet</label>
+        </div>
+        <div>
+          <select>
+            <option>Keto</option>
+            <option>Mediterranean</option>
+            <option>Paleo</option>
+            <option>Vegan</option>
+            <option>Carnivore</option>
+            <option>Vegetarian</option>
+          </select>
         </div>
 
         <div>
-            <label htmlFor="restrictions">Change your dietary restrictions here:</label>
-        </div>
+          <form>
+            <label>Select Nutrients to Track</label>
 
-        <div>
+            <div>
+              <input type="checkbox" id="calories" value="Calories"></input>
+              <label htmlFor="calories">Calories</label>
+              <input type="checkbox" id="Fat" value="Fat"></input>
+              <label htmlFor="Fat">Fat</label>
+              <input type="checkbox" id="Cholestrol" value="Cholestrol"></input>
+              <label htmlFor="Cholestrol">Cholestrol</label> <br></br>
+              <input type="checkbox" id="Sodium" value="Sodium"></input>
+              <label htmlFor="Sodium">Sodium</label>
+              <input
+                type="checkbox"
+                id="Carbohydrates"
+                value="Carbohydrates"
+              ></input>
+              <label for="Carbohydrates">Carbohydrates</label>
+              <input
+                type="checkbox"
+                id="Dietary Fiber"
+                value="Dietary Fiber"
+              ></input>
+              <label htmlFor="Dietary Fiber">Dietary Fiber</label> <br></br>
+              <input type="checkbox" id="Protein" value="Protein"></input>
+              <label htmlFor="Protein">Protein</label>
+              <input type="checkbox" id="Calcium" value="Calcium"></input>
+              <label htmlFor="Calcium">Calcium</label>
+              <input type="checkbox" id="Iron" value="Iron"></input>
+              <label htmlFor="Iron">Iron</label>
+            </div>
             <input
-                className="shadow appearance-none border rounded w-30% py-10 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="text" id="restrictions" name="restrictions" placeholder="previous restrictions"></input>
+              type="submit"
+              formaction="#"
+              value="Enter"
+              class="border border-3 mt-5"
+            ></input>
+          </form>
         </div>
-        <div>
-            <label htmlFor="dropdown">Select Diet</label>
-        </div>
-        <div>
-            <select>
-                <option>Keto</option>
-                <option>Mediterranean</option>
-                <option>Paleo</option>
-                <option>Vegan</option>
-                <option>Carnivore</option>
-                <option>Vegetarian</option>
-            </select>
-        </div>
-
-        <div>
-            <form>
-                <label>Select Nutrients to Track</label>
-
-                <div>
-                    <input type="checkbox" id="calories" value="Calories"></input>
-                    <label for="calories">Calories</label>
-                    <input type="checkbox" id="Fat" value="Fat"></input>
-                    <label for="Fat">Fat</label>
-                    <input type="checkbox" id="Cholestrol" value="Cholestrol"></input>
-                    <label for="Cholestrol">Cholestrol</label> <br></br>
-                    <input type="checkbox" id="Sodium" value="Sodium"></input>
-                    <label for="Sodium">Sodium</label>
-                    <input type="checkbox" id="Carbohydrates" value="Carbohydrates"></input>
-                    <label for="Carbohydrates">Carbohydrates</label>
-                    <input type="checkbox" id="Dietary Fiber" value="Dietary Fiber"></input>
-                    <label for="Dietary Fiber">Dietary Fiber</label> <br></br>
-                    <input type="checkbox" id="Protein" value="Protein"></input>
-                    <label for="Protein">Protein</label>
-                    <input type="checkbox" id="Calcium" value="Calcium"></input>
-                    <label for="Calcium">Calcium</label>
-                    <input type="checkbox" id="Iron" value="Iron"></input>
-                    <label for="Iron">Iron</label>
-                </div>
-                <input type="submit" formaction="#" value="Enter" class="border border-3 mt-5"></input>
-            </form>
-        </div>
-        </body>
-    );
+      </div>
+    </body>
+  );
 }
 
 export default AccountPage;
--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import {auth} from './firebase';
-import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth';
+import React from "react";
+import { auth } from "./firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import SignInForm from "./SignInForm";
 import Page from "./Page";
 import NavBar from "./NavBar";
@@ -10,130 +13,145 @@ import HistoryPage from "./HistoryPage";
 import RegisterForm from "./RegisterForm";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            signedIn: false,
-            page: Page.SignIn
-        }
-        this.signIn = this.signIn.bind(this);
-        this.register = this.register.bind(this);
-        this.signOut = this.signOut.bind(this);
-        this.changePage = this.changePage.bind(this);
-        this.authStateChanged = this.authStateChanged.bind(this);
-        auth.onAuthStateChanged(this.authStateChanged);
+  constructor(props) {
+    super(props);
+    this.state = {
+      signedIn: false,
+      page: Page.SignIn,
+    };
+    this.signIn = this.signIn.bind(this);
+    this.register = this.register.bind(this);
+    this.signOut = this.signOut.bind(this);
+    this.changePage = this.changePage.bind(this);
+    this.authStateChanged = this.authStateChanged.bind(this);
+    auth.onAuthStateChanged(this.authStateChanged);
+  }
+
+  signIn(email, password) {
+    signInWithEmailAndPassword(auth, email, password).catch((err) => {
+      let msg;
+      switch (err.code) {
+        case "auth/invalid-email":
+          msg = "Invalid email";
+          break;
+        case "auth/user-disabled":
+          msg = "Account has been disabled";
+          break;
+        case "auth/user-not-found":
+          msg = "Account not found";
+          break;
+        case "auth/wrong-password":
+          msg = "Incorrect password";
+          break;
+        case "auth/invalid-login-credentials":
+          msg = "Invalid login credentials";
+          break;
+        default:
+          msg = "Unknown error";
+      }
+      this.setState({
+        signInError: msg,
+      });
+    });
+  }
+
+  register(email, password, confirmPassword) {
+    if (password !== confirmPassword) {
+      this.setState({
+        registerError: "Passwords do not match",
+      });
+      return;
     }
 
-    signIn(email, password) {
-        signInWithEmailAndPassword(auth, email, password).catch(err => {
-            let msg;
-            switch (err.code) {
-                case 'auth/invalid-email':
-                    msg = "Invalid email";
-                    break;
-                case 'auth/user-disabled':
-                    msg = "Account has been disabled";
-                    break;
-                case 'auth/user-not-found':
-                    msg = "Account not found";
-                    break;
-                case 'auth/wrong-password':
-                    msg = "Incorrect password";
-                    break;
-                case 'auth/invalid-login-credentials':
-                    msg = 'Invalid login credentials';
-                    break;
-                default:
-                    msg = "Unknown error";
-            }
-            this.setState({
-                signInError: msg
-            });
-        });
+    createUserWithEmailAndPassword(auth, email, password).catch((err) => {
+      let msg;
+      switch (err.code) {
+        case "auth/email-already-in-use":
+          msg =
+            "Email already in use. If you already have an account, please sign in instead.";
+          break;
+        case "auth/invalid-email":
+          msg = "Invalid email";
+          break;
+        case "auth/weak-password":
+          msg = "Password is too weak";
+          break;
+        default:
+          msg = "Unknown error";
+      }
+      this.setState({
+        registerError: msg,
+      });
+    });
+  }
+
+  signOut() {
+    // noinspection JSIgnoredPromiseFromCall
+    auth.signOut();
+  }
+
+  changePage(page) {
+    this.setState({ page });
+  }
+
+  authStateChanged(user) {
+    if (user) {
+      this.setState({
+        signedIn: true,
+        signInError: undefined,
+        registerError: undefined,
+        page: Page.Home,
+      });
+    } else {
+      this.setState({
+        signedIn: false,
+        page: Page.SignIn,
+      });
     }
+  }
 
-    register(email, password, confirmPassword) {
-        if (password !== confirmPassword) {
-            this.setState({
-                registerError: "Passwords do not match"
-            });
-            return;
-        }
-
-        createUserWithEmailAndPassword(auth, email, password).catch(err => {
-            let msg;
-            switch (err.code) {
-                case 'auth/email-already-in-use':
-                    msg = "Email already in use. If you already have an account, please sign in instead.";
-                    break;
-                case 'auth/invalid-email':
-                    msg = "Invalid email";
-                    break;
-                case 'auth/weak-password':
-                    msg = "Password is too weak";
-                    break;
-                default:
-                    msg = "Unknown error";
-            }
-            this.setState({
-                registerError: msg
-            })
-        });
-    }
-
-    signOut() {
-        // noinspection JSIgnoredPromiseFromCall
-        auth.signOut();
-    }
-
-    changePage(page) {
-        this.setState({page});
-    }
-
-    authStateChanged(user) {
-        if (user) {
-            this.setState({
-                signedIn: true,
-                signInError: undefined,
-                registerError: undefined,
-                page: Page.Home
-            });
-        } else {
-            this.setState({
-                signedIn: false,
-                page: Page.SignIn
-            });
-        }
-    }
-
-    render() {
-        let element;
-        switch (this.state.page) {
-            case Page.SignIn:
-                return <SignInForm callback={this.signIn} register={() => this.changePage(Page.Register)} error={this.state.signInError}/>;
-            case Page.Register:
-                return <RegisterForm callback={this.register} signIn={() => this.changePage(Page.SignIn)} error={this.state.registerError}/>;
-            case Page.Home:
-                element = <HomePage/>;
-                break;
-            case Page.Account:
-                element = <AccountPage/>;
-                break;
-            case Page.History:
-                element = <HistoryPage/>;
-                break;
-            default:
-                element = <div className="flex text-5xl h-screen justify-center items-center text-red-500">Invalid page</div>;
-        }
+  render() {
+    let element;
+    switch (this.state.page) {
+      case Page.SignIn:
         return (
-            <>
-                <NavBar changePage={this.changePage} signOut={this.signOut}/>
-                <div className="p-5">
-                    {element}
-                </div>
-            </>
-        )
+          <SignInForm
+            callback={this.signIn}
+            register={() => this.changePage(Page.Register)}
+            error={this.state.signInError}
+          />
+        );
+      case Page.Register:
+        return (
+          <RegisterForm
+            callback={this.register}
+            signIn={() => this.changePage(Page.SignIn)}
+            error={this.state.registerError}
+          />
+        );
+      case Page.Home:
+        element = <HomePage />;
+        break;
+      case Page.Account:
+        element = <AccountPage />;
+        break;
+      case Page.History:
+        element = <HistoryPage />;
+        break;
+      default:
+        element = (
+          <div className="flex text-5xl h-screen justify-center items-center text-red-500">
+            Invalid page
+          </div>
+        );
     }
+    return (
+      <>
+        <NavBar changePage={this.changePage} signOut={this.signOut} />
+        <div className="p-5">{element}</div>
+      </>
+    );
+  }
 }
 
 export default App;

15d0e474ee9a2062f4c51502f9ee2f16e9d41630
Isaac Liu <xnscdev@gmail.com>
2023-10-29 11:21:47 -0500

    Add buttons for generating new recipe and going back to main prompt

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, {useRef, useState} from "react";
 import OpenAI from "openai";
-import { auth, db } from "./firebase";
-import { doc, collection, getDoc, setDoc } from "firebase/firestore";
+import {auth, db} from "./firebase";
+import {doc, collection, getDoc, setDoc} from "firebase/firestore";
 
 const openai = new OpenAI({
     apiKey: process.env.REACT_APP_OPENAI_API_KEY,
@@ -14,14 +14,14 @@ function HomePage() {
     const [recipe, setRecipe] = useState(null);
     const [confirmation, setConfirmation] = useState('');
     const [saved, setSaved] = useState(false);
+    const messages = useRef([]);
 
-    const getRecipe = async () => {
+    const getNewRecipe = async () => {
         const uid = auth.currentUser.uid;
         let userDoc;
         try {
             userDoc = await getDoc(doc(db, "users", uid));
-        }
-        catch (e) {
+        } catch (e) {
             setConfirmation(`Error reading your dietary preferences: ${e}`);
         }
         let dietaryRestrictions = "no dietary restrictions",
@@ -49,7 +49,7 @@ function HomePage() {
             if (tempNutrients.length > 0)
                 nutrients = tempNutrients.join(', ');
         }
-        const messages = [
+        messages.current = [
             {
                 role: "system",
                 content:
@@ -79,10 +79,26 @@ function HomePage() {
                     "Never output any other context or explanation; just output the contents of the JSON file.",
             },
         ];
+        await getRecipe();
+    }
+
+    const getAnotherRecipe = async () => {
+        setConfirmation('');
+        messages.current = [
+            {
+                role: "user",
+                content: "Please generate another recipe. It can be similar or completely unrelated to any of the previous recipes you've generated. Output this new recipe in the same format as before, and remember to just output the contents of the JSON file and not output any other context or explanation."
+            },
+            ...messages.current
+        ];
+        await getRecipe();
+    }
+
+    const getRecipe = async () => {
         setGenerating(true);
         const response = await openai.chat.completions.create({
             model: "gpt-3.5-turbo",
-            messages,
+            messages: messages.current,
         });
         const output = response.choices[0];
         if (output.finish_reason !== "stop") {
@@ -90,24 +106,27 @@ function HomePage() {
             return;
         }
 
+        messages.current = [output.message, ...messages.current];
         setConfirmation('');
         setGenerating(false);
         const r = JSON.parse(output.message.content);
         r['instructions'] = r['instructions'].map(item => item.replace(/^\d+\.\s+/, ''));
         setRecipe(r);
+        setSaved(false);
     };
 
     const saveRecipe = async () => {
-        if (saved)
+        if (saved) {
+            setConfirmation('This recipe has already been saved');
             return;
+        }
         const uid = auth.currentUser.uid;
         try {
             const c = collection(db, "users", uid, "history");
             await setDoc(doc(c), recipe);
             setConfirmation('Successfully saved recipe!');
             setSaved(true);
-        }
-        catch (e) {
+        } catch (e) {
             setConfirmation(`Error saving recipe: ${e}`);
         }
     };
@@ -127,11 +146,27 @@ function HomePage() {
                 <h3 className="text-center text-3xl font-medium my-10 text-blue-900">Here's your recipe:</h3>
                 <h3 className="text-center text-5xl font-medium my-10 text-blue-900">{recipe.name}</h3>
                 <div className="text-center space-x-10">
-                    <button onClick={saveRecipe} className="bg-green-600 hover:bg-green-400 rounded mb-10 text-white text-xl px-6 py-3">Save Recipe</button>
-                    <button className="bg-blue-500 hover:bg-blue-300 rounded mb-10 text-white text-xl px-6 py-3">Generate Another Recipe</button>
-                    <button className="bg-orange-500 hover:bg-orange-300 rounded mb-10 text-white text-xl px-6 py-3">Try a Different Dish</button>
+                    <button
+                        onClick={saveRecipe}
+                        className="bg-green-600 hover:bg-green-400 rounded mb-10 text-white text-xl px-6 py-3">
+                        Save Recipe
+                    </button>
+                    <button
+                        onClick={getAnotherRecipe}
+                        className={
+                            "rounded mb-10 text-white text-xl px-6 py-3 " + (generating
+                                ? "cursor-not-allowed bg-blue-300"
+                                : "focus:ring-blue-300 bg-blue-500 hover:bg-blue-300")}
+                        disabled={generating}
+                    >{generating ? 'Generating...' : 'Generate Another Recipe'}</button>
+                    <button
+                        onClick={() => setRecipe(null)}
+                        className="bg-orange-500 hover:bg-orange-300 rounded mb-10 text-white text-xl px-6 py-3"
+                    >Try a Different Dish
+                    </button>
                 </div>
-                <div className={"text-center mb-10 text-lg " + (confirmation.includes("Error") ? "text-red-500" : "text-green-700")}>{confirmation}</div>
+                <div
+                    className={"text-center mb-10 text-lg " + (confirmation.includes("Error") ? "text-red-500" : "text-green-700")}>{confirmation}</div>
                 <div className="flex flex-wrap lg:w-2/3 mx-auto text-2xl mb-24">
                     <div className="w-full xl:w-1/2 px-8">
                         <h4 className="text-center text-3xl text-white font-medium mb-10 bg-gradient-to-r from-blue-500 via-blue-800 to-blue-500 p-4">Nutrients</h4>
@@ -146,42 +181,45 @@ function HomePage() {
                         <ol className="list-decimal list-inside text-justify space-y-8">{instructions}</ol>
                     </div>
                 </div>
-
             </>
         );
-    }
-    else {
+    } else {
         content = (
             <div className="text-center">
-                <h2 className="text-5xl font-medium my-10 text-blue-900">
-                    Welcome! What would you like to eat today?
-                </h2>
-                <div className="w-1/2 mx-auto">
-                    <input
-                        type="text"
-                        id="text-input"
-                        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring text-2xl focus:ring-blue-500"
-                        placeholder="Enter a food..."
-                        value={input}
-                        onChange={e => setInput(e.target.value)}
-                    />
-                </div>
-                <div className="mt-8 text-center">
-                    <button
-                        type="button"
-                        className={
-                            "w-1/3 text-2xl text-white py-2 px-4 rounded-md focus:outline-none focus:ring" +
-                            (generating
-                                ? "cursor-not-allowed bg-blue-400"
-                                : "focus:ring-blue-300 bg-blue-600 hover:bg-blue-700")
-                        }
-                        onClick={getRecipe}
-                        disabled={generating}
-                    >
-                        {generating ? "Generating..." : "Generate Recipes!"}
-                    </button>
-                </div>
-                <div className={"text-center mt-6 text-lg " + (confirmation.includes("Error") ? "text-red-500" : "text-green-700")}>{confirmation}</div>
+                <form onSubmit={async e => {
+                    e.preventDefault();
+                    await getNewRecipe();
+                }}>
+                    <h2 className="text-5xl font-medium my-10 text-blue-900">
+                        Welcome! What would you like to eat today?
+                    </h2>
+                    <div className="w-1/2 mx-auto">
+                        <input
+                            type="text"
+                            id="text-input"
+                            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring text-2xl focus:ring-blue-500"
+                            placeholder="Enter a food..."
+                            value={input}
+                            onChange={e => setInput(e.target.value)}
+                        />
+                    </div>
+                    <div className="mt-8 text-center">
+                        <button
+                            type="submit"
+                            className={
+                                "w-1/3 text-2xl text-white py-2 px-4 rounded-md focus:outline-none focus:ring" +
+                                (generating
+                                    ? "cursor-not-allowed bg-blue-400"
+                                    : "focus:ring-blue-300 bg-blue-600 hover:bg-blue-700")
+                            }
+                            disabled={generating}
+                        >
+                            {generating ? "Generating..." : "Generate Recipes!"}
+                        </button>
+                    </div>
+                    <div
+                        className={"text-center mt-6 text-lg " + (confirmation.includes("Error") ? "text-red-500" : "text-green-700")}>{confirmation}</div>
+                </form>
             </div>
         );
     }
